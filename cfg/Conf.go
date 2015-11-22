ff528c21384c836823e3f7beb5ae47b696c7dbb1
lkj01010 <lkj01010@163.com>
2015-11-23 01:17:51 +0800

    add some websock test files

---
+++ cfg/Conf.go
@@ -0,0 +1,9 @@
+package cfg
+
+type Configuration struct {
+	Port int
+	Delay string
+	Interval string
+	TotalSize int
+	OnlyTestConnect bool
+}
--- chatserver.go
+++ chatserver.go
@@ -1,1 +1,75 @@
 package main
+
+import (
+
+
+	"golang.org/x/net/websocket"
+	"time"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"encoding/json"
+	"os"
+	"mygo01/cfg"
+)
+
+var Config cfg.Configuration
+
+func load(configfile string) cfg.Configuration {
+	config := cfg.Configuration{}
+	file, _ := os.Open(configfile)
+	decoder := json.NewDecoder(file)
+	err := decoder.Decode(&config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return config
+}
+
+func wsHandler(ws *websocket.Conn) {
+//	//p := unsafe.Pointer(&ws)
+//	//index := ((int)(uintptr(p))) % n
+//	index := rand.Intn(n)
+//	lock := locks[index]
+//	lock.Lock()
+//	wsList[index].PushBack(ws)
+//	lock.Unlock()
+//
+//	for {
+//		var reply string
+//		if err := websocket.Message.Receive(ws, &reply); err != nil {
+//			fmt.Println("Can't receive because of " + err.Error())
+//			break
+//		}
+//	}
+//
+//	lock.Lock()
+//	for e := wsList[index].Front(); e != nil; e = e.Next() {
+//		if e.Value.(*websocket.Conn) == ws {
+//			wsList[index].Remove(e)
+//			break
+//		}
+//	}
+//	lock.Unlock()
+}
+
+func main() {
+	seed := time.Now().UTC().UnixNano()
+	rand.Seed(seed)
+
+	Config = load("config.json")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		s := websocket.Server{Handler: websocket.Handler(wsHandler)}
+		s.ServeHTTP(w, req)
+	})
+
+	err := http.ListenAndServe(":"+strconv.Itoa(Config.Port), nil)
+	if err != nil {
+		panic("Error: " + err.Error())
+	}
+
+}
+
+
